1944694714b17fddff80818b490805df2d5e8acb
Alexandrine-L <alexandrineluseau@gmail.com>
2021-06-09 22:12:27 +0200

    1er commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BouteillesComponent } from './composants/bouteilles/bouteilles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BouteillesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/composants/bouteilles/bouteilles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bouteilles',
+  templateUrl: './bouteilles.component.html',
+  styleUrls: ['./bouteilles.component.css']
+})
+export class BouteillesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
